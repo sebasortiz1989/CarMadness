77dd623820ec87c557b0ece08c79d0f1757145c1
Juan@SDSharp <sebasortizsanta@gmail.com>
2021-05-21 23:22:37 -0500

    Sebas: [add] Added ManagerLocator.cs to access other managers

---
+++ Assets/Scripts/Managers/ManagerLocator.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class ManagerLocator : MonoBehaviour
+{
+    //monobehaviours
+    public UIManager _uiManager;
+
+    //singletons
+
+    public static ManagerLocator Instance
+    {
+        get
+        {
+            if (_instance == null) 
+                _instance = new ManagerLocator();
+            return _instance;
+        }
+        set { _instance = value; }
+    }
+    private static ManagerLocator _instance;
+
+    public ManagerLocator()
+    {
+
+    }
+}
